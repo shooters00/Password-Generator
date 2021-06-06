334c3b894ad760bbfbf84e6dcdbfa4e117980d74
shooters00 <shooters0000@gmail.com>
2021-06-06 12:30:46 -0400

    Code working.  Modified to use strings versus arrays for potential characters

--- archive/oldscript.js
+++ archive/oldscript.js
@@ -2,29 +2,36 @@
 var generateBtn = document.querySelector("#generate");
 
 //Define an array and string to store the resulting password
-//var passArray = [];
+var passArray = [];
 var passString = "";
-var inputObject = {};
 
 //Define min/max length for password
 const min = 8;
 const max = 128;
 
-const chars2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const nums2 = "0123456789";
-const specials2 = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~\"\\";
-var whatCharLength = "";  
+//Define upper case list and put in array
+const charList = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+const chars = charList.split(" ");
 
-var charsLower2 = [];
-for (var i = 0; i < chars2.length; i++) {
-    charsLower2 = charsLower2 + (chars2[i].toLowerCase());
+//Convert upper case list to lower and put in array
+const charsLower = [];
+for (var i = 0; i < chars.length; i++) {
+    charsLower.push(chars[i].toLowerCase());
 }
 
+//Define nums and put in array
+const numList = "0 1 2 3 4 5 6 7 8 9";
+const nums = numList.split(" ");
+
+//Define special characters and put in array.  Took out using a " and \. <- Can we fix?
+const specialList = " ! # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ] ^ _ ` { | } ~ \" \\"  
+const specials = specialList.split(" ");
+
 passProperties = {
-  chars2,
-  charsLower2,
-  nums2,
-  specials2
+  chars,
+  charsLower,
+  nums,
+  specials
 }
 
 //Collect Input from User & return an object with all input.  Rerun the function if validation criteria is not met.
@@ -36,19 +43,17 @@ function collectInput() {
     whatCharLength = prompt ("Enter a password length between " + min + " - " + max + " characters");
   }
   //Types of characters wanted
-    wantLowers = window.confirm ("Do you want lowercase characters in your password? (i.e. - " + charsLower2);
-    wantUppers = window.confirm ("Do you want uppercase characters in your password? (i.e. - " + chars2);
-    wantNums = window.confirm ("Do you want numeric characters in your password?(i.e. - " + nums2);
-    wantSpecials = window.confirm ("Do you want special characters in your password?(i.e. - " + specials2);
-  while (!wantLowers && !wantUppers && !wantNums && !wantSpecials) {
-    wantLowers = window.confirm ("Do you want lowercase characters in your password? (i.e. - " + charsLower2);
-    wantUppers = window.confirm ("Do you want uppercase characters in your password? (i.e. - " + chars2);
-    wantNums = window.confirm ("Do you want numeric characters in your password?(i.e. - " + nums2);
-    wantSpecials = window.confirm ("Do you want special characters in your password?(i.e. - " + specials2);
+  wantLowers = window.confirm ("Do you want lowercase characters in your password? (i.e. - " + charsLower);
+  wantUppers = window.confirm ("Do you want uppercase characters in your password? (i.e. - " + chars);
+  wantNums = window.confirm ("Do you want numeric characters in your password?(i.e. - " + nums);
+  wantSpecials = window.confirm ("Do you want special characters in your password?(i.e. - " + specials);
+
+  //Ensure at least one type of character was selected
+  if (!wantLowers && !wantUppers && !wantNums && !wantSpecials) {
     alert("You did not choose any character types.  A password cannot be generated.  Try again.");
+    collectInput();
   }
 
-  inputObject = {};
   //Return an object containing all responses
   return inputObject = {
     whatCharLength,
@@ -62,34 +67,33 @@ function collectInput() {
 //Generate the password
 function generatePassword() {
   let tempArray = [];
-  tempString = "";
   //Determine which questions were true and form a bank of character options to choose from randomly
   if (inputObject.wantLowers) {
     //start populating a temporary array
-    tempString = tempString.concat(passProperties.charsLower2);
+    tempArray = tempArray.concat(passProperties.charsLower);
   }
   if (inputObject.wantUppers) {
-    tempString = tempString.concat(passProperties.chars2);
+    tempArray = tempArray.concat(passProperties.chars);
   }
   if (inputObject.wantNums) {
-    tempString = tempString.concat(passProperties.nums2);
+    tempArray = tempArray.concat(passProperties.nums);
   }
   if (inputObject.wantSpecials) {
-    tempString = tempString.concat(passProperties.specials2);
+    tempArray = tempArray.concat(passProperties.specials);
   }
-  console.log("Potential characters to choose from: ");
-  console.log(tempString);
+  console.log("Potential characters to choose from: ")
+  console.log(tempArray);
 
   passArray = [];
   passString = "";
   //Need to pick a random number use math random and math floor against the length of characters and grab that array item
     for (var i=0; i < whatCharLength; i++) {
-      passString = passString + tempString[Math.floor(Math.random() * tempString.length)];
+      passArray.push(tempArray[Math.floor(Math.random() * tempArray.length)]);
     }
 
   //Do I need to return an array or just a string?
-  //console.log(passString);
-  //passString = JSON.stringify(passArray);
+  console.log(passArray);
+  passString = JSON.stringify(passArray);
   console.log("Resulting password generated: ")
   console.log(passString);
   return passString;
@@ -112,3 +116,5 @@ writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
